351874cc61267e6f92404c7fe310cdb5576c868c
Richard Kindler <223166946+RickCreator87@users.noreply.github.com>
2026-01-25 05:08:57 -0700

    Create src/app.ts

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express";
+import bodyParser from "body-parser";
+import webhookRoute from "./routes/webhook";
+
+const app = express();
+
+app.use(bodyParser.json({ verify: (req: any, _res, buf) => { req.rawBody = buf; }}));
+
+app.use("/webhook", webhookRoute);
+
+export default app;
